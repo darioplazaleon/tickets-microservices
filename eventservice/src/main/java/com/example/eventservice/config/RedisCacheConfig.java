d5cafe7dac9ab3fac909f5ee8e189142b95dfe61
darioplazaleon <darioplaza02@hotmail.com>
2025-05-21 15:31:39 -0300

    working transfer ticket with redis

--- eventservice/src/main/java/com/example/eventservice/config/RedisCacheConfig.java
+++ eventservice/src/main/java/com/example/eventservice/config/RedisCacheConfig.java
@@ -1,5 +1,7 @@
 package com.example.eventservice.config;
 
+import com.example.shared.data.EventSimpleData;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
@@ -19,22 +22,38 @@ import java.time.Duration;
 @EnableCaching
 public class RedisCacheConfig {
 
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class")
+    private static abstract class EventSimpleDataMixIn {}
+
     @Bean
-    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public ObjectMapper redisObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
+
+        objectMapper.addMixIn(EventSimpleData.class, EventSimpleDataMixIn.class);
 
         PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
                 .allowIfSubType(Object.class)
                 .build();
+
         objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL, com.fasterxml.jackson.annotation.JsonTypeInfo.As.PROPERTY);
+        return objectMapper;
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory, ObjectMapper redisObjectMapper) {
+        GenericJackson2JsonRedisSerializer jacksonSerializer = new GenericJackson2JsonRedisSerializer(redisObjectMapper);
+        RedisSerializationContext.SerializationPair<Object> jacksonSerializationPair =
+                RedisSerializationContext.SerializationPair.fromSerializer(jacksonSerializer);
 
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofHours(1))
-                .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair
-                                .fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper))
-                )
+                .serializeValuesWith(jacksonSerializationPair)
                 .disableCachingNullValues();
 
         return RedisCacheManager.builder(redisConnectionFactory)
--- ticketservice/src/main/java/com/example/ticketservice/config/RedisConfig.java
+++ ticketservice/src/main/java/com/example/ticketservice/config/RedisConfig.java
@@ -1,5 +1,11 @@
 package com.example.ticketservice.config;
 
+import com.example.shared.data.TicketData;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -8,6 +14,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
@@ -16,25 +23,47 @@ import java.util.Map;
 @Configuration
 public class RedisConfig {
 
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class")
+    private static abstract class TicketDataMixIn {}
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory();
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory cf) {
+    public ObjectMapper redisObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        objectMapper.addMixIn(TicketData.class, TicketDataMixIn.class);
+
+        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType(Object.class)
+                .build();
+        objectMapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL, com.fasterxml.jackson.annotation.JsonTypeInfo.As.PROPERTY);
+        return objectMapper;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory cf, ObjectMapper redisObjectMapper) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(cf);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(redisObjectMapper));
         return template;
     }
 
     @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory cf) {
+    public RedisCacheManager cacheManager(RedisConnectionFactory cf, ObjectMapper redisObjectMapper) {
+        GenericJackson2JsonRedisSerializer jacksonSerializer = new GenericJackson2JsonRedisSerializer(redisObjectMapper);
+        RedisSerializationContext.SerializationPair<Object> jacksonSerializationPair =
+                RedisSerializationContext.SerializationPair.fromSerializer(jacksonSerializer);
+
         RedisCacheConfiguration defaultCfg = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(10))
-                .disableCachingNullValues();
+                .disableCachingNullValues()
+                .serializeValuesWith(jacksonSerializationPair);
 
         Map<String, RedisCacheConfiguration> cfgs = Map.of(
                 "userTickets", defaultCfg.entryTtl(Duration.ofMinutes(5)),
